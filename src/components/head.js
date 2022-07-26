0f6e1b5c7944d3e466417762a034c515cdc38fb7
Grant Wecker <grantwecker@live.com>
2022-07-26 16:19:58 -0400

    Site description

--- src/components/head.js
+++ src/components/head.js
@@ -6,6 +6,7 @@ const Head = () => {
             <html lang="en" />
             <meta charset="UTF-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="Grant Wecker is a software developer currently focusing on web applications."></meta>
             
             {/* fontawesome -- for icons */}
             <script src="https://kit.fontawesome.com/169a9b2fc6.js" crossorigin="anonymous"></script>  
