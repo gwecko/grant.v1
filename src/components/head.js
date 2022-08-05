9e93f48e5e0a244a1e6af0be65b682a907d0ce2f
Grant Wecker <grantwecker@live.com>
2022-08-05 19:49:10 -0400

    Removed fontawesome

--- src/components/head.js
+++ src/components/head.js
@@ -5,12 +5,9 @@ const Head = () => {
         <head>
             <html lang="en" />
             <meta charset="UTF-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover'" />
             <meta name="description" content="Grant Wecker is a software developer currently focusing on web applications."></meta>
             <meta name="robots" content="all"></meta>
-            
-            {/* fontawesome -- for icons */}
-            <script src="https://kit.fontawesome.com/169a9b2fc6.js" crossorigin="anonymous"></script>  
         </head>
     )
 }
