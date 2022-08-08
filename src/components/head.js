68c1d0c87b58b43cf44ab72e2fa0b8da1ccb4d7f
Grant Wecker <grantwecker@live.com>
2022-08-08 16:14:21 -0400

    Attempted iphone notch fix #7

--- src/components/head.js
+++ src/components/head.js
@@ -7,7 +7,7 @@ const Head = () => {
             <meta charset="UTF-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
             <meta name="apple-mobile-web-app-capable" content="yes" />
-            <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
             <meta name="description" content="Grant Wecker is a software developer currently focusing on web applications." />
             <meta name="robots" content="all" />
         </head>
